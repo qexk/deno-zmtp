6db04f35ac65c678b32382831de2af08a5092ec5
Alexandre Szymocha <alexandre@szymocha.com>
2019-11-05 15:36:59 +0100

    Create abstract class Zmtp
    Provide two methods:
    - `greet` to connect to ZMQ.
    - `handshake` to initiate the authentication. Will be dispatched to a
      policy class.

---
+++ lib/Zmtp.ts
@@ -0,0 +1,28 @@
+/*
+    This file is part of Deno-ZMTP, an implementation of ZMTP for Deno.
+    Copyright (C) 2019 Contributors as noted in the AUTHORS.md file
+
+    Deno-ZMTP is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    As a reminder, the Contributors give you permission to dynamically
+    link this library from an executable, regardless of the license terms
+    of the executable, and to copy and distribute the executable under
+    terms of your choice.
+
+    Deno-ZMTP is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser
+    General Public License along with Deno-ZMTP.  If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+export default abstract class Zmtp {
+  public abstract greet(): Promise<void>;
+  public abstract handshake(): Promise<void>;
+}
