eb8acdd160666b6d5861ef5aa83d07ba49e3fbe8
Alexandre Szymocha <alexandre@szymocha.com>
2019-11-05 15:43:13 +0100

    Create main module export file
    Export types and values that can be useful for consumers.
    Export a builder function to inject the Policies into the Zmtp instance.

---
+++ mod.ts
@@ -0,0 +1,51 @@
+/*
+    This file is part of Deno-ZMTP, an implementation of ZMTP for Deno.
+    Copyright (C) 2019 Contributors as noted in the AUTHORS.md file
+
+    Deno-ZMTP is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    As a reminder, the Contributors give you permission to dynamically
+    link this library from an executable, regardless of the license terms
+    of the executable, and to copy and distribute the executable under
+    terms of your choice.
+
+    Deno-ZMTP is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser
+    General Public License along with Deno-ZMTP.  If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+import Zmtp_ from './lib/Zmtp.ts';
+import Zerializable_ from './lib/Zmtp/Zerializable.ts';
+import Command_ from './lib/Zmtp/Command.ts';
+import Null_ from './lib/Zmtp/Null.ts';
+import * as Detail from './lib/Zmtp/detail.ts';
+
+type Zmtp = Zmtp_;
+
+namespace Zmtp {
+  export const MAJOR = Detail.MAJOR;
+  export const MINOR = Detail.MINOR;
+
+  export type Zerializable = Zerializable_;
+  export const Command = Command_;
+  export const Null = Null_;
+
+  export async function build<Kind extends Zmtp>(
+    ZmtpKind: new (sock: Deno.Conn) => Kind,
+    sock: Deno.Conn,
+  ) {
+    const k: Kind = new ZmtpKind(sock);
+    await k.greet();
+    await k.handshake();
+  }
+}
+
+export default Zmtp;
